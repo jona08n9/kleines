43ee3ddb7cb0b376af8f9fbe82450e65e5302914
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-12-13 13:58:37 +0100

    2nd commit

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link rel="stylesheet" href="https://use.typekit.net/ijw0ado.css">
 		<link rel="stylesheet" href="custom.css">
 
 		<?php wp_head(); ?>
