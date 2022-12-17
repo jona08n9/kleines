7db6eaeb8a3d4faff7001aa8c5d5ac34ba5d67a3
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-12-17 21:17:30 +0100

    Icons and styling for footer done

--- footer.php
+++ footer.php
@@ -34,7 +34,7 @@
 						}
 						?>
 
-						<p class="powered-by-wordpress"> <span> - FARVEGLADE HÅNDLAVEDE SMYKKER </span></p>
+						<p class="powered-by-wordpress">- FARVEGLADE HÅNDLAVEDE SMYKKER</p>
 
 					</div><!-- .footer-credits -->
 
@@ -42,13 +42,13 @@
 						<span class="to-the-top-long">
 							<?php
 							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+							printf( __( 'TIL TOPPEN %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
 							?>
 						</span><!-- .to-the-top-long -->
 						<span class="to-the-top-short">
 							<?php
 							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+							printf( __( 'TIL TOPPEN %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
 							?>
 						</span><!-- .to-the-top-short -->
 					</a><!-- .to-the-top -->
