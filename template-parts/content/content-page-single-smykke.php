66ba8aff6b34eab6dca19748557412525ae7721e
TobiasRoland123 <samu1493@stud.kea.dk>
2022-12-19 23:41:39 +0100

    links rettet

--- template-parts/content/content-page-single-smykke.php
+++ template-parts/content/content-page-single-smykke.php
@@ -114,7 +114,7 @@
 
 	
 
-	const url="http://tobiasroland.dk/kea/10_eksamensopgave/kleines_tobias_domain/wordpress/wp-json/wp/v2/smykke/"+<?php echo get_the_ID() ?>;
+	const url="https://madvigux.dk/kleines/wp-json/wp/v2/smykke/"+<?php echo get_the_ID() ?>;
 		async function getJson(){
 			console.log("id er", <?php echo get_the_ID() ?> )
 			let response = await fetch(url);
